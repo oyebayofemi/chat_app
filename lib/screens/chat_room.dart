72392b14b7be4ef7eae53fec07fbbfac87ccaf85
oyebayofemi <61966651+oyebayofemi@users.noreply.github.com>
2022-04-22 13:07:18 +0100

    adding search delegate

--- lib/screens/chat_room.dart
+++ lib/screens/chat_room.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/screens/search_delegate.dart';
 import 'package:chat_app/services.dart/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -18,7 +19,7 @@ class _ChatRoomPageState extends State<ChatRoomPage> {
         actions: [
           IconButton(
               onPressed: () {
-                //showSearch(context: context, delegate: delegate);
+                showSearch(context: context, delegate: searchDelegate());
               },
               icon: Icon(Icons.search)),
           PopupMenuButton(
---
+++ lib/screens/search_delegate.dart
@@ -0,0 +1,108 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class searchDelegate extends SearchDelegate {
+  CollectionReference collectionReference =
+      FirebaseFirestore.instance.collection('users');
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    return super.appBarTheme(context).copyWith(
+          appBarTheme: super.appBarTheme(context).appBarTheme.copyWith(
+                elevation: 0.0,
+                // backgroundColor: Color(0xff991F36),
+              ),
+        );
+  }
+
+  @override
+  List<Widget>? buildActions(BuildContext context) {
+    return [
+      IconButton(
+          onPressed: () {
+            query = '';
+          },
+          icon: Icon(Icons.clear))
+    ];
+  }
+
+  @override
+  Widget? buildLeading(BuildContext context) {
+    return IconButton(
+        onPressed: () {
+          close(context, null);
+        },
+        icon: Icon(Icons.arrow_back));
+  }
+
+  @override
+  Widget buildResults(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      stream: collectionReference
+          .orderBy('id', descending: false)
+          .snapshots()
+          .asBroadcastStream(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return ListView(
+            children: [
+              ...snapshot.data!.docs
+                  .where((QueryDocumentSnapshot<Object?> element) =>
+                      element['name']
+                          .toString()
+                          .toLowerCase()
+                          .contains(query.toLowerCase()))
+                  .map((QueryDocumentSnapshot<Object?> data) {
+                final String username = data.get('name');
+                final String email = data.get('email');
+                final String url = data.get('pictureModel');
+
+                return url == null
+                    ? Center(
+                        child: CircularProgressIndicator(),
+                      )
+                    : Card(
+                        elevation: 0,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Row(
+                            children: [
+                              CircleAvatar(backgroundImage: NetworkImage(url)),
+                              Expanded(
+                                child: Column(
+                                  children: [Text('$username'), Text('$email')],
+                                ),
+                              ),
+                              Container(
+                                padding: EdgeInsets.all(10),
+                                decoration: BoxDecoration(
+                                  color: Colors.green,
+                                  borderRadius: BorderRadius.circular(50.r),
+                                ),
+                                // color: Colors.green,
+                                child: Text('Message'),
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+              })
+            ],
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    );
+  }
+
+  @override
+  Widget buildSuggestions(BuildContext context) {
+    return Center(
+      child: Text('Search for user'),
+    );
+  }
+}
